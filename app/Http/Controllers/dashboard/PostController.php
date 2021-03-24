e258ff801ec1349af9aef9839895a054ac48a7bb
Jp-droid-crypto <72044036+Jp-droid-crypto@users.noreply.github.com>
2021-03-24 13:48:47 -0500

    paginacion

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -23,7 +23,7 @@ class PostController extends Controller
     }
     public function index()
     {
-        $posts = Post::orderBy('created_at','desc')->paginate(5);
+        $posts = Post::orderBy('created_at','desc')->paginate(10);
         return view ('dashboard.post.index',['posts' => $posts]);
     }
 
