873f6b3452ff996534f0ddb28f773bd260b4a9bf
Jp-droid-crypto <72044036+Jp-droid-crypto@users.noreply.github.com>
2021-05-05 16:18:13 -0500

    origin

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,9 +3,18 @@
 @section('content')
     <div class="container">
        <div class="row">
+          <div class="col-md-12">
+              <video muted loop autoplay class="video">
+                  <source src="{{asset('images/videos/vid1.mp4')}}" type="video/mp4">
+                  <source src="movie.ogg" type="video/ogg">
+                  Your browser does not support the video tag.
+              </video>
+          </div>
+       </div>
+       <div class="row">
           <div class="col-md-3" v-for="post in posts">
                <figure class="figure">
-                     <img v-bind:src=" '/images/' + post.images" class="card-img-top" alt="...">
+                     <img v-bind:src=" '/images/' + post.images" class="img figure-img img-fluid" alt="...">
                </figure>
                <figcaption>
                   <p class="texto">@{{post.content}}</p>
